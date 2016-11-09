748e7f1f0bc0896300e4672ee4cd5feb79e76c88
Georgi Hristozov <georgi@forkbomb.nl>
2016-11-09 22:54:04 +0200

    Update READMEs.

--- exercises/05/01derive-n.scm
+++ exercises/05/01derive-n.scm
@@ -6,6 +6,11 @@
 (define (derive-n f n)
   ((repeat-n derive n) f))
 
+; Грешен вариант на намирането на n-та производна. Това е n-та композиция на
+; първата производна.
+(define (derive-n-INCORRECT f n)
+  (repeat-n (derive f) n))
+
 (assert-approx 12 0.1 ((derive-n (lambda (x) (* x x x)) 1) 2))
 (assert-approx 12 0.1 ((derive-n (lambda (x) (* x x x)) 2) 2))
 (assert-approx 6 0.1 ((derive-n (lambda (x) (* x x x)) 3) 2))
