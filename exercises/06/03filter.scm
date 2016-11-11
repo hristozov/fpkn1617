7cc7c7c60d8997905c2d8b3a2f54deed88ab322a
Georgi Hristozov <georgi@forkbomb.nl>
2016-11-11 18:48:19 +0200

    Add a sample map implementation.

--- exercises/06/03filter.scm
+++ exercises/06/03filter.scm
@@ -11,5 +11,14 @@
       (else (helper (cdr current-l) result))))
   (helper l '()))
 
+; Примерна реализация на map.
+(define (my-map f l)
+  (if (null? l)
+    '()
+    (cons (f (car l))
+          (my-map f (cdr l)))))
+
 (assert-equal '(1 3 5 7) (filter odd? '(1 2 3 4 5 6 7 8)))
 (assert-equal '() (filter odd? '()))
+
+(assert-equal '(1 4 9 16 25) (my-map (lambda (x) (* x x)) '(1 2 3 4 5)))
