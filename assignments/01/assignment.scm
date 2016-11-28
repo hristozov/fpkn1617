485a16b5597d3bfee15376cb18f7924c8fd50591
Georgi Hristozov <georgi@forkbomb.nl>
2016-11-28 18:07:55 +0200

    Solution for task 1.

--- assignments/01/assignment.scm
+++ assignments/01/assignment.scm
@@ -1,7 +1,15 @@
 (load "../../lib/scm/unit.scm")
 
+; Конструира списък с цифрите на числото. В случая са в обратен ред, но за нас
+; това не е от значение.
+(define (digits n)
+  (if (<= n 0)
+      '()
+      (cons (remainder n 10) (digits (quotient n 10)))))
+
+; Сумира цифрите по указания начин, ползвайки filter.
 (define (sum-digits p n)
-  0)
+  (apply + (filter p (digits n))))
 
 (assert= 12 (sum-digits even? 123456))
 (assert= 9 (sum-digits odd? 123456))
