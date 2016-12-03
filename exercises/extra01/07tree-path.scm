d7cd1099833d8960ca05d07a27bb7cd0025075a0
Georgi Hristozov <georgi@forkbomb.nl>
2016-12-03 14:30:10 +0200

    Use the existing tree implementation.

--- exercises/extra01/07tree-path.scm
+++ exercises/extra01/07tree-path.scm
@@ -1,21 +1,4 @@
-(define (left tree) (cadr tree))
-
-(define (right tree) (caddr tree))
-
-(define (value tree) (car tree))
-
-(define (make-tree value left right)
-  (list value left right))
-
-(define (empty-tree) '())
-
-(define (tree-empty? tree)
-  (null? tree))
-
-(define (leaf? tree)
-  (and (not (tree-empty? tree))
-       (tree-empty? (left tree))
-       (tree-empty? (right tree))))
+(load "../08/05tree.scm")
 
 (define tree
   '(1
