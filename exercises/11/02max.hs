cd49b9986b136552242412c00c832dde95fa060c
Georgi Hristozov <georgi@forkbomb.nl>
2016-12-22 12:32:51 +0200

    Exercise 11.

---
+++ exercises/11/02max.hs
@@ -0,0 +1,19 @@
+import Unit
+
+-- първи вариант - с if
+max' a b = if a > b
+  then a
+  else b
+
+-- втори вариант - с guard
+-- забележете индентирането пред | - без него няма да работи
+max2' a b
+  | a > b = a
+  | otherwise = b
+
+main = do {
+  assertEqual 1 (max' 1 0);
+  assertEqual 4 (max' 1 4);
+  assertEqual 4 (max2' 1 4);
+  assertEqual 1 (max2' 1 0)
+}
---
+++ exercises/11/03factorial.hs
@@ -0,0 +1,23 @@
+import Unit
+
+-- първи вариант - с if
+fact n = if n == 1
+  then 1
+  else n * fact (n - 1)
+
+-- втори вариант - с guard
+-- забележете индентирането пред | - без него няма да работи
+fact2 n
+  | n == 1 = 1
+  | otherwise = n * fact2 (n - 1)
+
+-- трети вариант - с pattern matching
+fact3 1 = 1
+fact3 n = n * fact3 (n - 1)
+
+main = do {
+  assertEqual 2 (fact 2);
+  assertEqual 24 (fact 4);
+  assertEqual 2 (fact2 2);
+  assertEqual 24 (fact2 4)
+}
